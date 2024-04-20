2825147cc294433ce519f78acc7157a6db52605a
KiranM189 <148932414+KiranM189@users.noreply.github.com>
2024-04-20 14:08:29 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,19 @@
+# No changes required here EXCEPT FOR TASK 5
+"""
+This is the client which reads from "shared-data/scraped_articles.csv" present in server.py file
+"shared-data/scraped_articles.csv" is mounted from server container
+"""
+import pandas as pd
+from socket import *
+
+serverName = "172.17.0.2"   #i need to add ip address of server container here
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+
+URL = input("Input the URL:")
+clientSocket.send(URL.encode()) 
+clientSocket.close()
+
+df = pd.read_csv("shared-data/scraped_articles.csv")
+print(df)
